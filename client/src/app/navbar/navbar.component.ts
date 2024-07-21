6617570c9865e3849d5ec13ef9bfa6ea6098cf7f
a-elhussein <a.elhussein61@gmail.com>
2024-07-21 19:50:44 +0300

    routes + toastr added

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -3,30 +3,34 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { NgIf } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   accountService = inject(AccountService)
+  private router = inject(Router)
+  private toastr = inject(ToastrService)
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: Response => {
-        console.log(Response);
-
+      next: _ => {
+        this.router.navigateByUrl('/members')
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
