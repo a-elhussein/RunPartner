6d256596cb08f6bff7ae89506b11e66bfe1cd978
a-elhussein <a.elhussein61@gmail.com>
2024-08-19 06:24:50 +0300

    work work work

--- Run Partner/obj/Debug/net8.0/Run Partner.AssemblyInfo.cs
+++ Run Partner/obj/Debug/net8.0/Run Partner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Run Partner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc6710d63bf38313be8fde53d7988d5b83a0dce6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9858f1a96de83785793a0cb296bd4ae37b8223")]
 [assembly: System.Reflection.AssemblyProductAttribute("Run Partner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Run Partner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
